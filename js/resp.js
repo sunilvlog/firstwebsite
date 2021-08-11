53ecb9933f5f1db5d093974936b22c4c7d6a3213
sunil singh <singhsunilwadwal@gmail.com>
2021-08-11 10:59:19 +0530

    firts commit

---
+++ js/resp.js
@@ -0,0 +1,15 @@
+var burger = document.querySelector('.burger')
+var navbar = document.querySelector('.navbar')
+var navList = document.querySelector('.nav-list')
+var rightNav = document.querySelector('.rightNav')
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+});
+
+$("#back").click(function (){
+    console.log("#About")
+    document.write("Thank You For Feedback!!!")
+})
